88faf5879ed3538bf5ab122fa6fea5c5a8c93b02
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-05-17 15:16:14 -0600

    Added steps 1-3

---
+++ justPy.py
@@ -0,0 +1,75 @@
+# Load necessary libraries
+from __future__ import print_function
+
+import math
+
+from IPython import display
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+
+# %tensorflow_version 1.x
+import tensorflow as tf
+
+#  (tf.python.data)
+from tensorflow.python.data import Dataset
+
+# tf.logging.ERROR and tf.logging.set_verbosity is deprecated
+# tf.logging.set_verbosity(tf.logging.ERROR)
+tf.compat.v1.logging.ERROR
+tf.compat.v1.logging.set_verbosity
+
+pd.options.display.max_rows = 10
+pd.options.display.float_format = '{:.1f}'.format
+
+
+# Load dataset
+california_housing_dataframe = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv", sep=",")
+
+'''  Randomize the data, just to be sure not to get any pathological ordering effects that might harm the performance of Stochastic Gradient Descent. Additionally, we'll scale median_house_value to be in units of thousands, so it can be learned a little more easily with learning rates in a range that we usually use. '''
+california_housing_dataframe = california_housing_dataframe.reindex(
+    np.random.permutation(california_housing_dataframe.index)
+)
+california_housing_dataframe["median_house_value"] /= 1000.0
+california_housing_dataframe.describe()
+
+
+''' Step 1: Define Features and Configure Feature Columns
+In order to import our training data into TensorFlow, we need to specify what type of data each feature contains. There are two main types of data we'll use in this and future exercises:
+Categorical Data: Data that is textual. In this exercise, our housing data set does not contain any categorical features, but examples you might see would be the home style, the words in a real-estate ad.
+Numerical Data: Data that is a number (integer or float) and that you want to treat as a number. As we will discuss more later sometimes you might want to treat numerical data (e.g., a postal code) as if it were categorical.
+In TensorFlow, we indicate a feature's data type using a construct called a feature column. Feature columns store only a description of the feature data; they do not contain the feature data itself.
+To start, we're going to use just one numeric input feature, total_rooms. The following code pulls the total_rooms data from our california_housing_dataframe and defines the feature column using numeric_column, which specifies its data is numeric: '''
+
+# Define the input feature total_rooms
+my_feature = california_housing_dataframe[["total_rooms"]]
+
+# Configure a numeric feature column for total_rooms.
+feature_columns = [tf.feature_column.numeric_column("total_rooms")]
+
+
+''' Step 2: Define the Target
+Next, we'll define our target, which is median_house_value. Again, we can pull it from our california_housing_dataframe: 
+'''
+# Define the label
+targets = california_housing_dataframe["median_house_value"]
+
+
+''' Step 3: Configure the LinearRegressor
+Next, we'll configure a linear regression model using LinearRegressor. We'll train this model using the GradientDescentOptimizer, which implements Mini-Batch Stochastic Gradient Descent (SGD). The learning_rate argument controls the size of the gradient step.
+NOTE: To be safe, we also apply gradient clipping to our optimizer via clip_gradients_by_norm. Gradient clipping ensures the magnitude of the gradients do not become too large during training, which can cause gradient descent to fail. '''
+
+# Use gradient descent as the optimizer for training the model.
+my_optimizer=tf.train.GradientDescentOptimizer(learning_rate=0.0000001)
+my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+
+# Configure the linear regression model with our feature columns and optimizer.
+# Set a learning rate of 0.0000001 for Gradient Descent.
+linear_regressor = tf.estimator.LinearRegressor(
+    feature_columns=feature_columns,
+    optimizer=my_optimizer
+)
+
